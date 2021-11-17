a2c30c1fe2f596d60ed0082c8ceb93d433a1741d
Dominik Ban <92435957+Beyzzor@users.noreply.github.com>
2021-11-17 12:14:20 +0100

    Add files via upload

---
+++ vjezba-14.py
@@ -0,0 +1,11 @@
+#Funkcije
+
+def mojaFunkcija():
+    print ("Ovo je 'Hello' iz funkcije.")
+
+mojaFunkcija()
+
+def pozdrav(ime):
+    print("Bok " + ime + ", danas je lijep dan.")
+
+pozdrav("Ban")
